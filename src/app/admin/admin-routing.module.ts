541728841d2a42a73c456dff1e081332754d8be5
Eric Baker <ericbaker@Erics-MacBook-Air.local>
2017-04-08 20:59:02 -0400

    add admin, email-blast, and users components

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from "app/admin/admin.component";
+import { EmailBlastComponent } from "app/admin/email-blast/email-blast.component";
+import { UsersComponent } from "app/admin/users/users.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent, children: [
+    {path: '', component: UsersComponent},
+    {path: 'blast', component: EmailBlastComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
